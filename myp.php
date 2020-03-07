9965002c7396a9e014bacb731027b034424b76c5
omidijibts <bukunmiomidiji@gmail.com>
2020-03-07 13:34:23 -0800

    another

---
+++ myp.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   Thank you, <?php echo $_GET["name"]; ?> <br>
+   Your email address is <?php echo $_GET["email"]; ?> 
+    
+</body>
+</html>
